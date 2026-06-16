f2ee3a10514bcb0d5e7e671bfa803de55df5d0a3
Matias Almaraz <mleandroalmaraz@gmail.com>
2026-06-16 20:30:33 -0300

    mapeo clases

--- back/src/main/java/com/example/appbitb2g/model/Concentracao.java
+++ back/src/main/java/com/example/appbitb2g/model/Concentracao.java
@@ -2,28 +2,42 @@ package com.example.appbitb2g.model;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Entity(name="concentracao")
 public class Concentracao {
+    @Id
+    @Column(name = "id")
     private Integer id; // SERIAL PK
+    @ManyToOne
+    @JoinColumn(name = "ecgi")
     private String ecgi; // FK -> antenas
     private String cluster;
     private String municipio;
+    @Column(name = "day_date")
     private LocalDate dayDate; // DATE
-    private String periodo; // MADRUGADA / MANHA / TARDE / NOITE
+    private String periodo; // MADRUGADA / MANHA / TARDE
+    @Column(name = "n_usuarios")
     private Integer nUsuarios; // INT
+    @Column(name = "download_gb")
     private Double downloadGb; // FLOAT
+    @Column(name = "congestionamento_medio")
     private Double congestionamentoMedio; // FLOAT
+    @Column(name = "rat_type_predominante")
     private String ratTypePredominante; // NR / LTE / WCDMA
-    
 }
--- back/src/main/java/com/example/appbitb2g/model/FlujoOd.java
+++ back/src/main/java/com/example/appbitb2g/model/FlujoOd.java
@@ -1,4 +1,8 @@
 package com.example.appbitb2g.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,14 +14,25 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Entity(name="flujo_od")
 public class FlujoOd {
+    @Id
+    @Column(name = "id")
     private Integer id; // SERIAL PK
+    @Column(name = "cluster_origem")
     private String clusterOrigem;
+    @Column(name = "cluster_destino")
     private String clusterDestino;
+    @Column(name = "municipio_origem")
     private String municipioOrigem;
+    @Column(name = "municipio_destino")
     private String municipioDestino;
+    @Column(name = "n_usuarios")
     private Integer nUsuarios; // INT
+    @Column(name = "n_viagens")
     private Integer nViagens; // INT
+    @Column(name = "dist_media_km")
     private Double distMediaKm; // FLOAT (64 bits -> Double en Java)
+    @Column(name = "mesmo_cluster")
     private Short mesmoCluster; // SMALLINT (0 / 1 -> Short en Java)
 }
