f0971d6ab1bed1bd57483097fb61108b614b0dba
Matias Almaraz <mleandroalmaraz@gmail.com>
2026-06-17 18:38:50 -0300

    fix : pom.xml

--- back/src/main/java/com/example/appbitb2g/service/SocialProgramService.java
+++ back/src/main/java/com/example/appbitb2g/service/SocialProgramService.java
@@ -1,5 +1,7 @@
 package com.example.appbitb2g.service;
 
+import java.time.LocalDate;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
