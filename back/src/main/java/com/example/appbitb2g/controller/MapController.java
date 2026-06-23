12bdc18848f77898d0f9fd9029d8aef55999afdf
GinaCapuchina <georginabosque@gmail.com>
2026-06-22 22:24:57 -0300

    feat: add CORS configurando controller para conectar frontend

--- back/src/main/java/com/example/appbitb2g/controller/MapController.java
+++ back/src/main/java/com/example/appbitb2g/controller/MapController.java
@@ -13,15 +13,13 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 
 @RestController
 @RequestMapping("/mapa")
+@CrossOrigin(origins = "*")
 @Tag(name = "Mapa", description = "Endpoints para visualizar regiones y sus indicadores territoriales")
 public class MapController{
     private final MapService mapService;
