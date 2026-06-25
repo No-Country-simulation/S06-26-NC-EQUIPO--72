d2fc56a2f39d8382b5590c40954aa094a2185f94
Matias Almaraz <mleandroalmaraz@gmail.com>
2026-06-25 11:34:39 -0300

    fix : cors

--- back/src/main/java/com/example/appbitb2g/repository/AntenaRepository.java
+++ back/src/main/java/com/example/appbitb2g/repository/AntenaRepository.java
@@ -2,7 +2,7 @@ package com.example.appbitb2g.repository;
 
 import com.example.appbitb2g.model.Antenna;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
 
 
 public interface AntenaRepository extends JpaRepository<Antenna, String> {
--- back/src/main/java/com/example/appbitb2g/repository/TensorOdSeederRepository.java
+++ back/src/main/java/com/example/appbitb2g/repository/TensorOdSeederRepository.java
@@ -1,8 +1,10 @@
 package com.example.appbitb2g.repository;
 
-import com.example.appbitb2g.model.Antenna;
 import com.example.appbitb2g.model.FlujoOd;
+
+
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface TensorOdSeederRepository extends JpaRepository<FlujoOd, String> {
+public interface TensorOdSeederRepository extends JpaRepository<FlujoOd, Integer> {
 }
