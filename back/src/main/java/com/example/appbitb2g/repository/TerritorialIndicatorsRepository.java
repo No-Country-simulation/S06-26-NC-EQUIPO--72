0b8ebde8685ea9c8bfea0396fe40b8ffb02e51ec
Matias Almaraz <mleandroalmaraz@gmail.com>
2026-06-29 11:02:58 -0300

    fix : congestionamiento medio

--- back/src/main/java/com/example/appbitb2g/repository/TerritorialIndicatorsRepository.java
+++ back/src/main/java/com/example/appbitb2g/repository/TerritorialIndicatorsRepository.java
@@ -48,7 +48,13 @@ public interface TerritorialIndicatorsRepository extends JpaRepository<Territori
                 i.municipio,
                 COALESCE(AVG(c_ant.lat), AVG(a_direct.lat)) as lat,               
                 COALESCE(AVG(c_ant.lon), AVG(a_direct.lon)) as lon,               
-                MAX(c.congestionamento_medio) as congestionamento_medio, 
+                ROUND(
+                    COALESCE(
+                        LN(NULLIF(AVG(c.congestionamento_medio), 0)) / 26.0,
+                        0
+                    ),
+                    4
+                ) as congestionamento_medio,
                 SUM(c.n_usuarios) as n_usuarios  
             FROM indicadores_territoriales i 
             LEFT JOIN concentracao c ON c.cluster = i.cluster
