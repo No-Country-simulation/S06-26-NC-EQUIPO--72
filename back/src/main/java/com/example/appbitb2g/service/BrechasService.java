21fdc1bb50d2b8cdb77cdb30919e50e407dc08da
Héctor Armando Cortez <updown.input@gmail.com>
2026-07-02 22:52:06 -0300

    feat: extend analizarBrechas method to include periodo and incomeCluster parameters

--- back/src/main/java/com/example/appbitb2g/service/BrechasService.java
+++ back/src/main/java/com/example/appbitb2g/service/BrechasService.java
@@ -3,5 +3,5 @@ package com.example.appbitb2g.service;
 import com.example.appbitb2g.dto.responseDTO.socialProgram.GapsResponseDTO;
 
 public interface BrechasService {
-    GapsResponseDTO analizarBrechas(String servicio, String municipio);
+    GapsResponseDTO analizarBrechas(String servicio, String municipio, String periodo, String incomeCluster);
 }
